13c127b6c4f7ef90ae7ebe8536af8723eeffb7be
Martin Jablecnik <martin.jablecnik@email.cz>
2019-01-20 14:33:04 +0100

    added loadin cards and setup at start game

--- Scenes/Card.gd
+++ Scenes/Card.gd
@@ -3,6 +3,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var type = ""
+var price = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Scripts/CardController.gd
+++ Scripts/CardController.gd
@@ -7,21 +7,65 @@ export (int) var SPACE_BETWEEN_CARDS = 20
 
 var card_scene = load("res://Scenes/Card.tscn")
 
-func add_card(position):
-    var card = card_scene.instance()
-    card.position = position
-    card.z_index = 100
-    card.get_node("Back").visible = false
-    card.get_node("Front").visible = true
-    add_child(card)
+
+
 
 
 func _ready():
+    var card_deck = load_cards()
+    
     var start_position = $"/root/Game/CardsPosition".position
-    start_position.x = -430
-    var width = get_node("Played").get_rect().size.x/2
-    print(width)
-    #var width = 70.0
+    start_position.x = -350
+    #print(selected_card.name, selected_card.type)
+
+    var width = 70.0
     for i in CARDS_NUM:
-        start_position.x += i + width + SPACE_BETWEEN_CARDS
-        add_card(start_position)
+        var card = card_deck[randi()%31+1].duplicate()
+        start_position.x += i + width + SPACE_BETWEEN_CARDS 
+        card.position = start_position 
+        print(card.position)
+        add_child(card)
+
+
+
+
+
+func create_card(card):
+    match card.type:
+        "builder":
+            card.get_node("Front").texture = load("res://Images/card_front_brown.png")
+            card.find_node("Currency").text = "bricks"
+        "soldier":
+            card.get_node("Front").texture = load("res://Images/card_front_green.png")
+            card.find_node("Currency").text = "weapons"
+        "wizard":
+            card.get_node("Front").texture = load("res://Images/card_front_purple.png")
+            card.find_node("Currency").text = "crystals"
+    card.position = $"/root/Game/CardsPosition".position
+    card.find_node("Name").text = card.name
+    card.find_node("Price").text = card.price
+    return card
+
+
+func load_cards():
+    var file = File.new()
+    file.open("res://Configs/cards.txt", File.READ)
+    var card_deck = []
+    
+    var line = file.get_line()
+    var variable_list = line.split('|')
+    while true:
+        line = file.get_line()
+        if file.eof_reached(): 
+            break
+        line = line.split('|')
+        var card = card_scene.instance()
+        card.name = line[1].strip_edges()
+        card.type = line[2].strip_edges()
+        card.price = line[3].strip_edges()
+        card_deck.append(create_card(card))
+    file.close()
+    return card_deck
+    
+    
+
