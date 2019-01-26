bdcff9147a1cd29a1352653969a6402c8e79b285
Martin Jablecnik <martin.jablecnik@email.cz>
2019-01-26 15:20:08 +0100

    improved play card

--- Scripts/Cards.gd
+++ Scripts/Cards.gd
@@ -11,30 +11,48 @@ var deck_cards = []
 var played_cards = []
 
 func _ready():    
-    var card_deck = $"./../CardDeck"
     all_cards = $"/root/Game/".load_cards()
     
-    for i in CARDS_NUM:
-        randomize()
-        var card = all_cards[randi()%29+1].duplicate()
-        card.position.x += (card.get_width() + SPACE_BETWEEN_CARDS) * i
-        deck_cards.append(card)
-        card_deck.add_child(card)
+    for i in CARDS_NUM:   
+        var card_position = Vector2(0.0, 0.0)
+        var card_width = all_cards[0].get_width()
+        card_position.x = (card_width + SPACE_BETWEEN_CARDS) * i  
+        add_new_card_to_deck(card_position)
 
 
 
 func play_card(card):
+    var card_position = card.position
+    remove_from_deck(card)
+    move_to_played(card)
+    card.is_played = true
+    
+    add_new_card_to_deck(card_position)
+    
+    
+func remove_from_deck(card):
     var card_deck = $"./../CardDeck"
     var index = deck_cards.find(card)
     deck_cards.remove(index)
-    
+    card_deck.remove_child(card)
+
+
+func move_to_played(card):
     var played = get_node("Played")
     card.position = Vector2(0.0, 0.0)
-    card_deck.remove_child(card)
     for child in played.get_children():
         played.remove_child(child)
+        
     played.add_child(card)
-    
     played_cards.append(card)
-    print(played_cards)
-    card.is_played = true
+    
+    
+func add_new_card_to_deck(card_position):
+    #var deck = get_node("Deck")
+    var card_deck = $"./../CardDeck"
+    randomize()
+    var card = all_cards[randi()%29+1].duplicate()
+    card.position = card_position
+    deck_cards.append(card)
+    card_deck.add_child(card)
+    
